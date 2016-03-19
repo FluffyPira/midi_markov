d4a1dc7b9524b599ea2e7f301bae5a924c4b1bb8
Elizabeth Wetton <FluffyPira@users.noreply.github.com>
2016-03-19 02:32:18 -0400

    super simplification

--- generator.rb
+++ generator.rb
@@ -15,6 +15,32 @@ SEQUENCE = MIDI::Sequence.new()
 @tracks = ARGV[3].to_i
 @length = ARGV[4].to_i
 
+# Round notes to only allow lengths we can deal with. 
+# Not as free as it was before but makes a lot more sense and sounds better.
+def rounded_note_length(delta, length)
+  return length if delta.include?(length)
+
+  # This is the bit that rounds the note. 
+  # Given a set of 2 note lengths in sequence, we know the lowest note can only ever be 0.5 the highest def note
+  # Thus, dead centre between the two is 0.75. Less, we round down. Higher we round up.
+  
+  delta.each_cons(2) do |x, y|
+    if length >= x && length <= y
+      if length / y < 0.7499
+        return x
+      else
+        return y
+      end
+    end
+  end
+end
+
+# How we figure out the length of a note. May be different based on temp but not sure. This library is silly.
+def delta(length)
+  value = SEQUENCE.note_to_delta(length)
+  return value
+end
+
 # Here's where we make our corpus of songs. It stores the midi number of the note (+64 because that's how this library works?)
 if ARGV[0].include?('store')
   
@@ -57,43 +83,34 @@ if ARGV[0].include?('store')
       f.puts corpus_notes
     }
     
+    delta_array = [delta("eighth"), delta("quarter"), delta("half"), delta("whole")]
+    
     ⏰ = notes.select { |x| x.channel == channels }.map(&:length)
-    corpus_length= ⏰.map(&:inspect).join(' ')
+    
+    time = []
+    
+    ⏰.each do |value|
+      delta = rounded_note_length(delta_array, value)
+      
+      if delta.nil?
+        delta = delta_array.sample
+      end
+      
+      time << delta
+    end
+    
+    corpus_length = time.map(&:inspect).join(' ')
     
     open('length_corpus.txt', 'a') { |f|
       f.puts corpus_length
     }
   end
 
-# Here is where we make music. Mostly lifted from the example that came with the library and heavily edited to do silly things.
-# This is a silly thing.  
+  # Here is where we make music. Mostly lifted from the example that came with the library and heavily edited to do silly things.
+  # This is a silly thing.  
 elsif ARGV[0].include?('build')
   
-  # Round notes to only allow lengths we can deal with. 
-  # Not as free as it was before but makes a lot more sense and sounds better.
-  def rounded_note_length(delta, length)
-    return length if delta.include?(length)
 
-    # This is the bit that rounds the note. 
-    # Given a set of 2 note lengths in sequence, we know the lowest note can only ever be 0.5 the highest def note
-    # Thus, dead centre between the two is 0.75. Less, we round down. Higher we round up.
-    
-    delta.each_cons(2) do |x, y|
-      if length >= x && length <= y
-        if length / y < 0.7499
-          return x
-        else
-          return y
-        end
-      end
-    end
-  end
-
-  # How we figure out the length of a note. May be different based on temp but not sure. This library is silly.
-  def delta(length)
-    value = SEQUENCE.note_to_delta(length)
-    return value
-  end
   
   # Initialize the midi file and name our song.
   track = Track.new(SEQUENCE)
@@ -117,16 +134,23 @@ elsif ARGV[0].include?('build')
   
   # Here we build our array of possible note lengths.
   # No idea how small we can get here.
-  delta_array = [delta("eighth"), delta("quarter"), delta("half"), delta("whole")]
+
   
   # Here's where we actually make music.
   @tracks.times do
     track_number += 1
     
-    # TO DO LATER: Better way of selecting how many notes there will be.
+    # Build our Markov chain makers
     note_markov = MidiMarkovMaker.new('note_corpus.txt')
+    length_markov = MidiMarkovMaker.new('length_corpus.txt')
+
+    # Build our markov generated note and length arrays
     notes = note_markov.create(@length)
-    
+    length = length_markov.create(@length)
+    final_notes = notes.split(" ").map(&:to_i)
+    final_length = length.split(" ").map(&:to_i)
+
+    # Set up our track information
     track = Track.new(SEQUENCE)
     SEQUENCE.tracks << track
 
@@ -140,26 +164,11 @@ elsif ARGV[0].include?('build')
 
     track.events << ProgramChange.new(0, 1, 0)
     
-    # Turn our notes into an array of itegers from a string.
-    notes2 = notes.split(" ").map(&:to_i)
-    
-    notes2.each do |note|
-      # Generate us some note lengths!
-      length_markov = MidiMarkovMaker.new('length_corpus.txt')
-      length = length_markov.create(5)
-      # Eliminate garbage, although this is kinda redundant now.
-      length2 = length.split(" ").map(&:to_i).delete_if { |a| a < delta("eighth") }.delete_if { |a| a > delta("whole") }
-      
-      if length2.empty?
-        length2 = delta_array
-      end
-      
-      # Get ourselves a rounded length.
-      length = rounded_note_length(delta_array, length2.sample)
-      
-      # Channel, Note, Velocity, Delta? 
-      track.events << NoteOn.new(0, note, 127, 0)
-      track.events << NoteOff.new(0, note, 127, length)
+    # build our notes
+    final_notes.each_with_index do |value, index|
+      # Channel, Note, Velocity, Delta?
+      track.events << NoteOn.new(0, value, 127, 0)
+      track.events << NoteOff.new(0, value, 127, final_length[index])
     end
   end
   
